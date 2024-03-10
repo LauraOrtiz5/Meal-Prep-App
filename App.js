659eaef3ad2782b6955c5a665c08394d26ecf965
LauraOrtiz5 <lortizclua@gmail.com>
2024-03-10 18:12:42 +0100

    Main page grid

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 
 export default function App() {
   return (
-    <CategoriesScreen />
+    <>
+      <StatusBar style='light'/>
+      <CategoriesScreen />
+    </>
   );
 }
 
