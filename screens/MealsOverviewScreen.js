077a463f1d84f4f6d591ddf98c67cb3f9f8fd54b
LauraOrtiz5 <lortizclua@gmail.com>
2024-03-16 16:07:10 +0100

    Meals list

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,26 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { MEALS } from '../data/dummy-data';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
-function MealsOverviewScreen() {
+import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/NewItem';
+
+function MealsOverviewScreen({ route }) {
+    const catID = route.params.categoryId;
+
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catID) >= 0;
+    });
+
+    function renderMealItem(itemData) {
+        return <MealItem title={itemData.item.title} />
+    }
+
     return (
         <View style={styles.container}>
-            <Text>
-                Meals Overview Screen
-            </Text>
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item) => item.id}
+                renderItem={renderMealItem}
+            />
         </View>
     );
 };
