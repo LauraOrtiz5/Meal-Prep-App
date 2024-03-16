8e34585054d3c468ecfe2eda65a1f6b38fec466a
LauraOrtiz5 <lortizclua@gmail.com>
2024-03-16 17:44:30 +0100

    Image and styling

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,7 +1,7 @@
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
-import MealItem from '../components/NewItem';
+import MealItem from '../components/MealItem';
 
 function MealsOverviewScreen({ route }) {
     const catID = route.params.categoryId;
@@ -11,8 +11,16 @@ function MealsOverviewScreen({ route }) {
     });
 
     function renderMealItem(itemData) {
-        return <MealItem title={itemData.item.title} />
-    }
+        return (
+            <MealItem
+                title={itemData.item.title}
+                imageUrl={itemData.item.imageUrl}
+                affordability={itemData.item.affordability}
+                complexity={itemData.item.complexity}
+                duration={itemData.item.duration}
+            />
+        );
+    };
 
     return (
         <View style={styles.container}>
